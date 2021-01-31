26fa8d953fa562da7a1ca6ec35b8fd762bb23b5d
Rana Alhmoud <ranaalhmoud92@gmail.com>
2021-01-31 20:32:35 +0200

    password crack finish

---
+++ index.js
@@ -0,0 +1,9 @@
+function crack() {
+  var pass = document.getElementById("passInput").value;
+  //    we want to take tha pass from the input box then crack it by split it and take each letter or index from this pass with all alphabets
+  //         one by one then return the cracking and we need to compare then stop when it matches
+
+  var alpha = "0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOIPASDFGHJKLZXCVBNM";
+
+  //aa .... ba , bb ....
+}
